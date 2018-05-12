ee2cec8cdfd0e0b27ee005c79c2cbee8b91f03a5
twittwoodley <twoodley1992@hotmail.co.uk>
2018-05-12 13:11:15 +0100

    v5

--- front-page.php
+++ front-page.php
@@ -7,6 +7,50 @@
 	</section>
 <?php
 	get_template_part('template-parts/content', 'navigation');
+?>
 
-
-	echo "Hello World";
+<div class="container">
+	<div class="inner-cont">
+		<h3>Brook</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus cursus ipsum et tincidunt consectetur. Donec tristique dignissim lobortis. Nulla ultricies lectus eget porta posuere. Phasellus et aliquam quam. Pellentesque ut laoreet lacus. Maecenas eu viverra mauris, efficitur ultricies velit. Nullam sollicitudin enim ex, non posuere orci pulvinar eget.
+</p>
+	</div>
+	<div class="inner-cont">
+		<h3>House</h3>
+		<p>Maecenas nec tortor ultricies, tempus mauris id, blandit ligula. Vivamus finibus lacus non sagittis pellentesque. Sed viverra placerat venenatis. Integer nulla erat, placerat id congue ut, pellentesque et quam. Duis imperdiet odio non sem eleifend, sed imperdiet diam sollicitudin. Integer at vulputate nisi. Suspendisse laoreet at dolor placerat mollis. Nullam egestas, mi ac porttitor posuere, lectus dui suscipit lorem, egestas rutrum justo ex eget nulla. Suspendisse euismod nisl egestas orci pellentesque, non placerat tortor convallis. Etiam at tellus id diam imperdiet malesuada. Aenean nec est ornare, varius magna sit amet, varius magna. Nullam dolor nisl, eleifend egestas ultrices et, placerat vel libero. Integer sodales nunc elit, commodo elementum nisi molestie sit amet. Sed bibendum felis pulvinar, egestas nunc a, laoreet arcu. Sed vestibulum, arcu sit amet faucibus laoreet, purus risus pharetra risus, nec viverra ante lacus et risus.
+		</p>
+	</div>
+	<div class="inner-cont">
+		<h3>Annexe</h3>
+		<p>Maecenas nec tortor ultricies, tempus mauris id, blandit ligula. Vivamus finibus lacus non sagittis pellentesque. Sed viverra placerat venenatis. Integer nulla erat, placerat id congue ut, pellentesque et quam. Duis imperdiet odio non sem eleifend, sed imperdiet diam sollicitudin. Integer at vulputate nisi. Suspendisse laoreet at dolor placerat mollis. Nullam egestas, mi ac porttitor posuere, lectus dui suscipit lorem, egestas rutrum justo ex eget nulla. Suspendisse euismod nisl egestas orci pellentesque, non placerat tortor convallis. Etiam at tellus id diam imperdiet malesuada. Aenean nec est ornare, varius magna sit amet, varius magna. Nullam dolor nisl, eleifend egestas ultrices et, placerat vel libero. Integer sodales nunc elit, commodo elementum nisi molestie sit amet. Sed bibendum felis pulvinar, egestas nunc a, laoreet arcu. Sed vestibulum, arcu sit amet faucibus laoreet, purus risus pharetra risus, nec viverra ante lacus et risus.
+		</p>
+	</div>
+</div>
+<h2>Why Exmouth?</h2>
+<div class="panels">
+    <div class="panel panel1">
+      <p>Hey</p>
+      <p>Let's</p>
+      <p>Dance</p>
+    </div>
+    <div class="panel panel2">
+      <p>Give</p>
+      <p>Take</p>
+      <p>Receive</p>
+    </div>
+    <div class="panel panel3">
+      <p>Experience</p>
+      <p>It</p>
+      <p>Today</p>
+    </div>
+    <div class="panel panel4">
+      <p>Give</p>
+      <p>All</p>
+      <p>You can</p>
+    </div>
+    <div class="panel panel5">
+      <p>Life</p>
+      <p>In</p>
+      <p>Motion</p>
+    </div>
+  </div>
